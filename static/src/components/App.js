a9155f1bb3da61611c40b576de673f552c037266
Egor Sorokin <egor@k15t.com>
2018-04-11 20:58:38 +0200

    Add personal data component

--- static/src/components/App.js
+++ static/src/components/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import PersonalData from './PersonalData';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        App Component is Rendered!!
+        <PersonalData />
       </div>
     );
   }
--- static/src/index.js
+++ static/src/index.js
@@ -1,6 +1,6 @@
 import { render } from 'react-dom';
 import React from 'react';
-import App from './component/App';
+import App from './components/App';
 import './style.scss';
 
 render(<App />, document.getElementById('app'));

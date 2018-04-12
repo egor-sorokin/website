e0a56bdacf92444bd978eed693ae293785dbed1a
Egor Sorokin <egor@k15t.com>
2018-04-12 12:26:03 +0200

    Add api request

--- static/src/utils/api.js
+++ static/src/utils/api.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
+
 export function getPersonalData() {
   return axios.get(`http://127.0.0.1:8080/api/v.1.0/person`);
+}
+
+
+export function getWorkExperience() {
+  return axios.get(`http://127.0.0.1:8080/api/v.1.0/work_experience`);
 }

41e086ec71f8227553cb3fd62edb44c43ac416ce
Egor Sorokin <>
2018-04-21 16:40:36 +0200

    Update dependencies

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Define the database (SQLite)
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 DATABASE_CONNECT_OPTIONS = {}
 
 # Application threads. A common general assumption is
@@ -17,3 +19,12 @@ THREADS_PER_PAGE = 2
 
 # Enable protection agains *Cross-site Request Forgery (CSRF)*
 CSRF_ENABLED     = True
+
+
+class TestingConfig(object):
+    TESTING = True
+    DEBUG = True
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    DEBUG_TB_ENABLED = True
+    PRESERVE_CONTEXT_ON_EXCEPTION = False

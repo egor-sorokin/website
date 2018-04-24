81aca24f23b09156fb805bed95fa9a05f76f8cda
Egor Sorokin <>
2018-04-24 07:35:36 +0200

    Add tests to check data in responses

--- tests/test_api.py
+++ tests/test_api.py
@@ -17,16 +17,27 @@ class TestAPI(BaseTestConfig):
         self.assertEqual(response.status_code, 404)
 
     # Test person endpoint
-    def test_get_person_route(self):
-        person = Person(first_name='Eg',
-                        last_name='Sor',
-                        email='example@example.com',
-                        phone='123456',
-                        summary='lorem ipsum')
+    def test_get_person(self):
+        initial_data = {
+            'first_name': 'Eg',
+            'last_name': 'Sor',
+            'email': 'example@example.com',
+            'phone': '123456',
+            'summary': 'lorem ipsum'
+        }
+
+        person = Person(first_name=initial_data['first_name'],
+                        last_name=initial_data['last_name'],
+                        email=initial_data['email'],
+                        phone=initial_data['phone'],
+                        summary=initial_data['summary'])
+
         db.session.add(person)
         db.session.commit()
         response = self.app.get('/api/v.1.0/person')
+        result = json.loads(response.data).get('personData')
         self.assertEqual(response.status_code, 200)
+        self.assertTrue(cmp(result, initial_data))
 
     def test_request_from_empty_person_table(self):
         db.session.query(Person).delete()
@@ -35,16 +46,26 @@ class TestAPI(BaseTestConfig):
         self.assertEqual(response.status_code, 404)
 
     # Test work experience endpoint
-    def test_get_work_experience_route(self):
-        work_experience = WorkExperience(company='Hand',
-                                         position='Developer',
-                                         date='11.02.11-12.02.13',
-                                         location='Whenever',
-                                         person_id='1')
+    def test_get_work_experience(self):
+        initial_data = {
+            'company': 'cooompany',
+            'position': 'pos',
+            'date': 'daaate',
+            'location': 'location',
+            'person_id': '1'
+        }
+
+        work_experience = WorkExperience(company=initial_data['company'],
+                                         position=initial_data['position'],
+                                         date=initial_data['date'],
+                                         location=initial_data['location'],
+                                         person_id=initial_data['person_id'])
         db.session.add(work_experience)
         db.session.commit()
         response = self.app.get('/api/v.1.0/work_experience')
+        result = json.loads(response.data).get('workExperience')
         self.assertEqual(response.status_code, 200)
+        self.assertTrue(cmp(result, initial_data))
 
     def test_request_from_empty_work_experience_table(self):
         db.session.query(WorkExperience).delete()
@@ -53,15 +74,24 @@ class TestAPI(BaseTestConfig):
         self.assertEqual(response.status_code, 404)
 
     # Test projects endpoint
-    def test_get_projects_route(self):
-        projects = Project(project='OlaOla',
-                           project_description='Lorem',
-                           project_url='www.example.com',
-                           work_experience_id='1')
-        db.session.add(projects)
+    def test_get_projects(self):
+        initial_data = {
+            'project': 'proj',
+            'project_description': 'desc',
+            'project_url': 'proj-url',
+            'work_experience_id': '1',
+        }
+
+        project = Project(project=initial_data['project'],
+                          project_description=initial_data['project_description'],
+                          project_url=initial_data['project_url'],
+                          work_experience_id=initial_data['work_experience_id'])
+        db.session.add(project)
         db.session.commit()
         response = self.app.get('/api/v.1.0/projects')
+        result = json.loads(response.data).get('projects')
         self.assertEqual(response.status_code, 200)
+        self.assertTrue(cmp(result, initial_data))
 
     def test_request_from_empty_projects_table(self):
         db.session.query(Project).delete()
@@ -70,14 +100,22 @@ class TestAPI(BaseTestConfig):
         self.assertEqual(response.status_code, 404)
 
     # Test skills endpoint
-    def test_get_skills_route(self):
-        skills = Skill(category='Soft',
-                       skills='lorem ipsum, ipsum lorem',
-                       person_id='1')
-        db.session.add(skills)
+    def test_get_skills(self):
+        initial_data = {
+            'category': 'cat',
+            'skills': 'ski',
+            'person_id': '1',
+        }
+
+        skill = Skill(category=initial_data['category'],
+                      skills=initial_data['skills'],
+                      person_id=initial_data['person_id'])
+        db.session.add(skill)
         db.session.commit()
         response = self.app.get('/api/v.1.0/skills')
+        result = json.loads(response.data).get('skills')
         self.assertEqual(response.status_code, 200)
+        self.assertTrue(cmp(result, initial_data))
 
     def test_request_from_empty_skills_table(self):
         db.session.query(Skill).delete()
@@ -87,15 +125,25 @@ class TestAPI(BaseTestConfig):
 
     # Test educations endpoint
     def test_get_educations_route(self):
-        educations = Education(place='Plaaace!',
-                               degree='Degreee',
-                               date='12-12-12',
-                               location='Lorem',
-                               person_id='1')
-        db.session.add(educations)
+        initial_data = {
+            'place': 'place',
+            'degree': 'degreedd',
+            'date': 'date',
+            'location': 'location',
+            'person_id': '1',
+        }
+
+        education = Education(place=initial_data['place'],
+                              degree=initial_data['degree'],
+                              date=initial_data['date'],
+                              location=initial_data['location'],
+                              person_id=initial_data['person_id'])
+        db.session.add(education)
         db.session.commit()
         response = self.app.get('/api/v.1.0/educations')
+        result = json.loads(response.data).get('educations')
         self.assertEqual(response.status_code, 200)
+        self.assertTrue(cmp(result, initial_data))
 
     def test_request_from_empty_educations_table(self):
         db.session.query(Education).delete()

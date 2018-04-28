c003ea20e4b65405029256ffdb391d044b951908
Egor Sorokin <>
2018-04-28 07:36:07 +0200

    Reset styles and add default

--- static/src/index.js
+++ static/src/index.js
@@ -1,6 +1,6 @@
 import { render } from 'react-dom';
 import React from 'react';
 import App from './components/App/index';
-import './style.scss';
+import './styles/index.scss';
 
 render(<App />, document.getElementById('app'));

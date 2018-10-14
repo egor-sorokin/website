24f68a3a11c4b0b7b171f024330727607c123774
Egor Sorokin <egor@k15t.com>
2018-10-14 15:45:07 +0200

    Add ordered list component

---
+++ static/src/components/OrderedList/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles.scss';
+
+
+const OrderedList = () => {
+  const orderedListItems = ['LinkedIn', 'Xing', 'GitHub', 'Codepen'].map((item) => (
+    <li className="orderedlist__item">
+      <span className="orderedlist__text">{item}</span>
+    </li>
+    
+  ));
+
+  return (
+    <div className="orderedlist">
+      <h5 className="orderedlist__title">Attachments</h5>
+      <ol className="orderedlist__list" start="1">
+        {orderedListItems}
+      </ol>
+    </div>
+  )
+};
+
+
+export default OrderedList;

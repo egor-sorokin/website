1cf2ce52f7f387418551bb711a9f06fc9f35dfb8
Egor Sorokin <egor@k15t.com>
2018-10-20 11:47:17 +0200

    Add linkMasked component

---
+++ static/src/components/LinkMasked/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.scss';
+
+
+const LinkStretched = ({linkData, cssClasses, target}) => (
+  <div className="link-masked">
+    <span className={'link-masked__placeholder ' + cssClasses}>{linkData.text}</span>
+    <div className="link-masked__mask">
+      <a
+        href={linkData.link}
+        target={target}
+        className={'link-masked__text ' + cssClasses}
+      >
+        {linkData.text}
+      </a>
+    </div>
+  </div>
+);
+
+LinkStretched.propTypes = {
+  linkData: PropTypes.shape({}),
+  cssClasses: PropTypes.string,
+  target: PropTypes.string
+};
+
+LinkStretched.defaultProps = {
+  linkData: {},
+  cssClasses: '',
+  target: ''
+};
+
+export default LinkStretched;

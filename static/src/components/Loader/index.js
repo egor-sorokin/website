5aad02702950ccfc6cf00acc1f3e056a21c47d14
Egor Sorokin <egor@k15t.com>
2018-10-31 22:13:04 +0100

    Add loader component

---
+++ static/src/components/Loader/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+const Loader = () => (
+  <div class="loader">
+    <h3 class="loader__inner font-s-20 text-c-mercury-light">
+      Loading...
+    </h3>
+  </div>
+);
+
+export default Loader;

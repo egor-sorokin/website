402c79f2bb0ef4859644c3975b514c585a62b13d
Egor Sorokin <egor@k15t.com>
2018-11-02 09:13:35 +0100

    Add a delay for the response

---
+++ static/src/components/Loader/animation.js
@@ -0,0 +1,11 @@
+import {Power2} from 'gsap';
+
+
+export const attachHideAnimation = (targetTween) => {
+  targetTween
+    .to('body', 0, {overflowY: 'hidden'})
+    .to('.loader__inner', .1,
+      {opacity: 0, ease: Power2.easeOut}
+    )
+    .to('body', 0, {overflowY: 'initial'});
+};
--- static/src/components/Loader/index.js
+++ static/src/components/Loader/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {TimelineMax} from 'gsap';
+import {attachHideAnimation} from './animation';
 import './styles.scss';
 
-const Loader = () => (
-  <div className="loader">
-    <h3 className="loader__inner font-s-20 text-c-l-dune">
-      Loading...
-    </h3>
-  </div>
-);
+class Loader extends Component {
+  constructor(props) {
+    super(props);
+
+    this.loaderTween = null;
+  }
+
+
+  componentDidMount() {
+    this.loaderTween = new TimelineMax({paused: true});
+    attachHideAnimation(this.loaderTween);
+  }
+
+
+  componentWillUnmount() {
+    this.loaderTween.play();
+  }
+
+
+  render() {
+    return (
+      <div className="loader">
+        <h3 className="loader__inner font-s-20 text-c-l-dune">
+          Loading...
+        </h3>
+      </div>
+    );
+  }
+}
 
 export default Loader;

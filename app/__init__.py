572662a1fef6b626d921dce83cbd9b54d525a44d
Egor Sorokin <egor@k15t.com>
2019-10-16 14:08:17 +0200

    Update requirements to resolve warnings about vulnerabilities

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, jsonify
 
 # Import SQLAlchemy
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 # Define the WSGI application object

bae88c5b8b7f492c5da41e1b0fb7937c8ed34ebb
Egor Sorokin <egor@k15t.com>
2020-05-07 22:05:58 +0200

    Get rid of useless abstractions

--- static/src/components/Pages/Experiments/index.js
+++ static/src/components/Pages/Experiments/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import InnerSlide from '../../InnerSlide';
+import DescriptionList from '../../DescriptionList';
+import ImageGrid from '../../ImageGrid';
+import Switcher from '../../Switcher';
+import '../../../styles/app/_common.scss';
 
 
 const Experiments = ({ experiments }) => (
-  <main id="experiments">
-    <InnerSlide
-      key="experiments"
-      slideName="experiments"
-      name={experiments.name}
-      info={experiments.info}
-      showcases={experiments.showcases}
-    />
+  <main id="experiments" key="experiments">
+    <section className="slide-inner">
+      <div className="container">
+        <section className="content content--experiments">
+          <div className="col col--left">
+            <h1 className="font-s-40">
+              {experiments.name}
+            </h1>
+            <DescriptionList
+              listData={experiments.info}
+            />
+          </div>
+          <div className="col col--right">
+            <ImageGrid
+              gridData={experiments.showcases}
+            />
+          </div>
+        </section>
+      </div>
+      <Switcher />
+    </section>
   </main>
 );
 
--- static/src/components/Pages/Projects/index.js
+++ static/src/components/Pages/Projects/index.js
@@ -2,23 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { SWITCHER_PROJECTS } from '../../../constants/index';
+import '../../../styles/app/_common.scss';
 import './styles.scss';
-import InnerSlide from '../../InnerSlide';
+import Switcher from '../../Switcher';
+import DescriptionList from '../../DescriptionList';
 
 
 const Projects = ({ projects }) => {
   const projectsComponent = projects.map(({
     id, name, info, image, url,
   }) => (
-    <InnerSlide
-      key={id}
-      slideName="projects"
-      name={name}
-      info={info}
-      image={image}
-      url={url}
-      switcherLink={SWITCHER_PROJECTS}
-    />
+    <section key={id} className="slide-inner">
+      <div className="container">
+        <section className="content content--projects">
+          <div className="col col--left">
+            <h1 className="font-s-40">
+              {name}
+            </h1>
+            <DescriptionList
+              listData={info}
+            />
+          </div>
+          <div className="col col--right">
+            <img
+              className="image"
+              title={image.alt}
+              alt={image.alt}
+              src={image.src}
+            />
+          </div>
+        </section>
+      </div>
+      <Switcher
+        url={url}
+        switcherLink={SWITCHER_PROJECTS}
+      />
+    </section>
   ));
 
   return (
